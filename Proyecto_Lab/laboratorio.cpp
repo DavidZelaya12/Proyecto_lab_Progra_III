c40fe5694630bdf92bfa461a7ee75b8e11c84e31
AbrahamReyes08 <124213092+AbrahamReyes08@users.noreply.github.com>
2023-12-14 09:00:20 -0600

    update

--- Proyecto_Lab/laboratorio.cpp
+++ Proyecto_Lab/laboratorio.cpp
@@ -1,6 +1,112 @@
 #include "laboratorio.h"
 
+const string &laboratorio::getLabSolicitado() const
+{
+    return labSolicitado;
+}
+
+const string &laboratorio::getClaseRequerida() const
+{
+    return claseRequerida;
+}
+
+const string &laboratorio::getMotivoDeUso() const
+{
+    return motivoDeUso;
+}
+
+const string &laboratorio::getPerfil() const
+{
+    return perfil;
+}
+
+const string &laboratorio::getRepeticion() const
+{
+    return repeticion;
+}
+
+const string &laboratorio::getNombreCompleto() const
+{
+    return nombreCompleto;
+}
+
+const string &laboratorio::getNumeroDeCuenta() const
+{
+    return numeroDeCuenta;
+}
+
+const string &laboratorio::getCorreo() const
+{
+    return correo;
+}
+
+int laboratorio::getCantIntegrantes() const
+{
+    return cantIntegrantes;
+}
+
+const string &laboratorio::getNombresYNumerosExtra() const
+{
+    return nombresYNumerosExtra;
+}
+
+const string &laboratorio::getEquipo() const
+{
+    return equipo;
+}
+
+const string &laboratorio::getFecha() const
+{
+    return fecha;
+}
+
+const string &laboratorio::getHorarioInicio() const
+{
+    return horarioInicio;
+}
+
+const string &laboratorio::getHorarioFin() const
+{
+    return horarioFin;
+}
+
 laboratorio::laboratorio()
+{
+    labSolicitado = "";
+    claseRequerida = "";
+    motivoDeUso = "";
+    perfil = "";
+    repeticion = "";
+    nombreCompleto = "";
+    numeroDeCuenta = "";
+    correo = "";
+    cantIntegrantes = 0;
+    nombresYNumerosExtra = "";
+    equipo = "";
+    fecha = "";
+    horarioInicio = "";
+    horarioFin = "";
+}
+
+laboratorio::laboratorio(std::string labSolicitado, std::string claseRequerida, std::string motivoDeUso, std::string perfil, std::string repeticion, std::string nombreCompleto, std::string numeroDeCuenta, std::string correo, int cantIntegrantes, std::string nombresYNumerosExtra, std::string equipo, std::string fecha, std::string horarioInicio, std::string horarioFin)
+{
+    this->labSolicitado = labSolicitado;
+    this->claseRequerida = claseRequerida;
+    this->motivoDeUso = motivoDeUso;
+    this->perfil = perfil;
+    this->repeticion = repeticion;
+    this->nombreCompleto = nombreCompleto;
+    this->numeroDeCuenta = numeroDeCuenta;
+    this->correo = correo;
+    this->cantIntegrantes = cantIntegrantes;
+    this->nombresYNumerosExtra = nombresYNumerosExtra;
+    this->equipo = equipo;
+    this->fecha = fecha;
+    this->horarioInicio = horarioInicio;
+    this->horarioFin = horarioFin;
+}
+
+laboratorio::~laboratorio()
 {
 
 }
--- Proyecto_Lab/laboratorio.h
+++ Proyecto_Lab/laboratorio.h
@@ -1,11 +1,48 @@
 #ifndef LABORATORIO_H
 #define LABORATORIO_H
+#include <iostream>
 
+using std::string;
 
 class laboratorio
 {
+private:
+    string labSolicitado;
+    string claseRequerida;
+    string motivoDeUso;
+    string perfil;
+    string repeticion;
+    string nombreCompleto;
+    string numeroDeCuenta;
+    string correo;
+    int cantIntegrantes;
+    string nombresYNumerosExtra;
+    string equipo;
+    string fecha;
+    string horarioInicio;
+    string horarioFin;
+
 public:
     laboratorio();
+    laboratorio(string labSolicitado, string claseRequerida, string motivoDeUso, string perfil,
+                string repeticion, string nombreCompleto, string numeroDeCuenta, string correo,
+                int cantIntegrantes, string nombresYNumerosExtra, string equipo, string fecha,
+                string horarioInicio, string horarioFin);
+    ~laboratorio();
+    const string &getLabSolicitado() const;
+    const string &getClaseRequerida() const;
+    const string &getMotivoDeUso() const;
+    const string &getPerfil() const;
+    const string &getRepeticion() const;
+    const string &getNombreCompleto() const;
+    const string &getNumeroDeCuenta() const;
+    const string &getCorreo() const;
+    int getCantIntegrantes() const;
+    const string &getNombresYNumerosExtra() const;
+    const string &getEquipo() const;
+    const string &getFecha() const;
+    const string &getHorarioInicio() const;
+    const string &getHorarioFin() const;
 };
 
 #endif // LABORATORIO_H
